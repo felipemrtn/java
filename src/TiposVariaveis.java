20196a678c1727286752a3c05253f1984714b321
felipemrtn <luiz1997martins@gmail.com>
2024-04-29 17:07:22 -0300

    Teste

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,9 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        int decremento = -3;
+        int aumento = 2;
+
+        int resultado = aumento + decremento;
+        System.out.println(resultado);
+    }
+}
